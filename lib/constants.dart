a5daa6fad01f3f7ce73cedb29ececb2472e6a692
Gustavo <gustavo.falarz@gmail.com>
2020-06-24 22:33:53 -0300

    Playing with color schemes.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,13 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-const Color colorPrimary = Color(0xff2E5902);
-const Color colorSecondary = Color(0xffDAF2C2);
-const Color colorContrast = Color(0xffBDD982);
+//const Color accent = Color(0xffff5722);
+//const Color colorSecondary = Color(0xffDAF2C2);
+//const Color colorPrimary = Color(0xff2E5902);
+//const Color colorContrast = Color(0xffBDD982);
 const Color colorMainText = Color(0x8A000000);
+
+const Color colorPrimary = Color(0xff455a64);
+const Color colorSecondary = Color(0xffffffff);
+const Color colorContrast = Color(0xffe1e2e1);
 const Color accent = Color(0xffff5722);
